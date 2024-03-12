9b5aa18c71ed9ebbc98fddf54007e7962bf66e91
iskandar arbi <162743214+Mugiida@users.noreply.github.com>
2024-03-12 12:47:15 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 function submitProblem() {
 	var problem = document.getElementById("problemInput").value;
 	localStorage.setItem("clientProblem", problem);
-	alert("Thank you for sharing your problem!");
+	alert("Thank you for sharing your problem!🌸");
 	document.getElementById("problemInput").value = "";
 }
 
@@ -10,4 +10,13 @@ window.onload = function () {
 	if (savedProblem) {
 		console.log("Client's problem: " + savedProblem);
 	}
+
+	let darkMode = false;
+
+	function toggleMode() {
+		darkMode = !darkMode;
+		document.body.classList.toggle("dark-mode", darkMode);
+	}
+
+	document.getElementById("toggleMode").addEventListener("click", toggleMode);
 };
